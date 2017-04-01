75c12ad9c0a41debc1338c4bb62001285473467c
Adam Deeley <ad.deeley@gmail.com>
2017-04-01 21:45:42 +0100

    Updated the Square constructor to just name

--- src.cpp
+++ src.cpp
@@ -5,32 +5,88 @@ using namespace std;
 
 class Piece{
 	public:
+		Piece( string nm ):
+			name{ nm }{};
 		string get_name(){ return name; };
 	private:
-		string name{"king"};
+		string name{};
 };
 
 class Square{
 	public:
-		Square( Square* pr = nullptr, Square* nxt = nullptr ):
-			prev{ pr }, next{ nxt }{};
+		Square( char nm ):
+			name{ nm } {};
 
 		//Piece* get_occupying_piece() { return p; };
-		Square* get_next() const { return next; };
-		Square* get_prev() const { return prev; };
-		Piece* pce {nullptr};
+		Piece* occupier {nullptr};
+		char get_name() { return name; };
+		Square* prev { nullptr };
+		Square* next { nullptr };
 	private:
-		Square* prev;
-		Square* next;
+		const char name;
 };
 
-int main(){
-	// create linked list
-	Square* sq = new Square();
-	Piece* p = new Piece();
+class Board{
+	public:
+		Board();
+		Square* find_square( string name );
+		void move_piece( string source, string destination );
+		Square* get_squarelist(){ return squarelist; };
+	private:
 
-	sq->pce = p;
-	cout << sq->pce->get_name();
+		Square* squarelist;
+};
+
+Board::Board(){
+	// Generates the linked list for board
+	string names = "abcdefghi";
+	for ( char c : names )
+	{
+		if ( c == 'a' ) // declare the list initially
+		{
+			Square* squarelist = new Square( c );
+			continue;
+		}
+		squarelist->next = new Square( c );
+		squarelist->next->prev = squarelist;
+		squarelist = squarelist->next;
+
+	}
+}
+
+Square* Board::find_square( string name ){
+	// finds the square object with called name and returns a pointer to tie object.
+	Square* sq = squarelist;
+	//find the first object in the linked list
+	while( sq -> prev )
+	{
+		sq = sq->prev;
+	}
+	/// to finish	
+}
+
+void test_board_constructor( Board b ){
+
+	Square* brd = b.get_squarelist();
+	while ( brd->prev )
+	{
+		cout << brd->get_name();
+		brd = brd->prev;
+	}
+}
+
+int main(){
+	Board b = Board();
+	test_board_constructor( b );
+	// create linked list
+	/*Square* sq = new Square();
+	sq.next = new Square();
+	sq.prev= new Square();
+	sq->occupier = new Piece( "Pawn" );
+	cout << sq->occupier->get_name() << '\n';
+	sq->next->occupier = new Piece( "King" );
+	cout << sq->next->occupier->get_name() << '\n';
+	*/
 	// check dest tile is free
 	// check source is free
 	// move pointer of dest to piece
